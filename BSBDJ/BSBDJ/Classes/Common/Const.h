b24f0728624949613a4713c77c477551fc29e2cc
yahaong <yahaong@gmail.com>
2016-05-29 16:36:51 +0800

    Add PrefixHeader.pch and Const

---
+++ BSBDJ/BSBDJ/Classes/Common/Const.h
@@ -0,0 +1,13 @@
+//
+//  Const.h
+//  BSBDJ
+//
+//  Created by yahaong on 16/5/29.
+//  Copyright © 2016年 yahaong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+UIKIT_EXTERN CGFloat const kTabBarH;
+UIKIT_EXTERN CGFloat const kStatusBarH;
+UIKIT_EXTERN CGFloat const kNavigationBarH;
---
+++ BSBDJ/BSBDJ/Classes/Common/Const.m
@@ -0,0 +1,13 @@
+//
+//  Const.m
+//  BSBDJ
+//
+//  Created by yahaong on 16/5/29.
+//  Copyright © 2016年 yahaong. All rights reserved.
+//
+
+#import "Const.h"
+
+CGFloat const kTabBarH = 49.0f;
+CGFloat const kStatusBarH = 20.0f;
+CGFloat const kNavigationBarH = 40.f;
